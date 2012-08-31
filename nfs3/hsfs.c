14318cc4b0841b355b25a57ded1fbe394a081451
mijinlong <mijinlong@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-08-31 01:28:07 +0000

    nfs3: add usage
    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8280 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- nfs3/hsfs.c
+++ nfs3/hsfs.c
@@ -1,30 +1,65 @@
 
-#define FUSE_USE_VERSION 26
-
-#include <fuse_lowlevel.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
 
 #include "hsfs.h"
+
+char *progname = NULL;
+char *mountspec = NULL;
 
 static struct fuse_lowlevel_ops hsfs_oper = {
 	.init		= hsx_fuse_init,
 	.destroy		= hsx_fuse_destroy,
 };
 
+static int 
+hsi_fuse_parse_cmdline(int argc, char **argv, struct fuse_args *args, char **udata)
+{
+
+	return 0;
+}
+
+void hsi_fuse_mount_usage()
+{
+	printf("usage: %s remotetarget dir [-rvVwfnh] [-t version] [-o enfsoptions]\n", progname);
+	printf("options:\n\t-r\t\tMount file system readonly\n");
+	printf("\t-v\t\tVerbose\n");
+	printf("\t-V\t\tPrint version\n");
+	printf("\t-w\t\tMount file system read-write\n");
+	printf("\t-f\t\tFake mount, don't actually mount\n");
+	printf("\t-n\t\tDo not update /etc/mtab\n");
+	printf("\t-s\t\tTolerate sloppy mount options rather than failing.\n");
+	printf("\t-h\t\tPrint this help\n");
+	printf("\tversion\t\tenfs - currently, the only choice\n");
+	printf("\tenfsoptions\tRefer mount.enfs(8) or enfs(5)\n\n");
+}
+
 int main(int argc, char **argv)
 {
-	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-	struct fuse_chan *ch;
-	char *mountpoint;
+	struct fuse_args args = {};
+	struct fuse_chan *ch = NULL;
+	char *mountpoint = NULL, *userdata = NULL;
 	int err = -1;
 
-	if (fuse_parse_cmdline(&args, &mountpoint, NULL, NULL) != -1 &&
-	    (ch = fuse_mount(mountpoint, &args)) != NULL) {
+	progname = basename(argv[0]);
+
+	if (argc < 3) {
+		hsi_fuse_mount_usage();
+		exit(-1);
+	}
+
+	mountspec = argv[1];
+	mountpoint = argv[2];
+	argc -= 3;
+
+	if (hsi_fuse_parse_cmdline(argc, &argv[3], &args, &userdata) != -1 &&
+		(ch = fuse_mount(mountpoint, &args)) != NULL) {
 		struct fuse_session *se;
 
 		se = fuse_lowlevel_new(&args, &hsfs_oper,
-				       sizeof(hsfs_oper), NULL);
+				       sizeof(hsfs_oper), userdata);
 		if (se != NULL) {
 			if (fuse_set_signal_handlers(se) != -1) {
 				fuse_session_add_chan(se, ch);
