b9489e646807bf17d14dcce7cefd008d5631a407
yanhuan <yanhuan@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-03 03:35:17 +0000

    log
    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8289 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- include/hsi_nfs3.h
+++ include/hsi_nfs3.h
@@ -4,3 +4,36 @@
 extern int hsi_nfs3_mkdir(struct hsfs_inode* parent, struct hsfs_inode** new, char* name, mode_t mode);
 
 extern int hsi_nfs3_rmdir(struct hsfs_inode* hsfs_inode, char* name);
+
+/**  
+* Remove a file
+*
+* @param hsfs_inode
+* @param name to remove
+*
+* @return error number
+**/
+extern int hsi_nfs3_remove(struct hsfs_inode *hi, const char *name);
+
+/**  
+* Rename a file
+*
+* @param source hsfs_inode
+* @param source name
+* @param target hsfs_inode
+* @param target name
+*
+* @return error number
+**/
+extern int hsi_nfs3_rename(struct hsfs_inode *hi, const char *name,
+ 		struct hsfs_inode *newhi, const char *newname);
+
+/**  
+* Change nfs3 status number to fuse error number
+*
+* @param nfs3 status number
+*
+* @return fuse error number
+**/
+extern int hsi_nfs3_stat_to_errno(int stat);
+
--- include/hsx_fuse.h
+++ include/hsx_fuse.h
@@ -10,3 +10,40 @@ extern int hsx_fuse_reply_entry(fuse_req_t req, struct hsfs_inode* hsfs_inode);
 extern (void *)hsx_fuse_rmdir(fuse_req_t req, fuse_ino_t parent, const char* name);
 
 
+/**  
+* Remove a file
+*
+* If the file's inode's lookup count is non-zero, the file
+* system is expected to postpone any removal of the inode
+* until the lookup count reaches zero (see description of the
+* forget function).
+*
+* Valid replies:
+*   fuse_reply_err
+*
+* @param req request handle
+* @param parent inode number of the parent directory
+* @param name to remove
+**/
+extern void hsx_fuse_unlink(fuse_req_t req, fuse_ino_t parent, const char *name);
+
+/** Rename a file
+ *
+ * If the target exists it should be atomically replaced. If
+ * the target's inode's lookup count is non-zero, the file
+ * system is expected to postpone any removal of the inode
+ * until the lookup count reaches zero (see description of the
+ * forget function).
+ *
+ * Valid replies:
+ *   fuse_reply_err
+ *
+ * @param req request handle
+ * @param parent inode number of the old parent directory
+ * @param name old name
+ * @param newparent inode number of the new parent directory
+ * @param newname new name
+ **/
+extern void hsx_fuse_rename(fuse_req_t req, fuse_ino_t parent, const char *name, 
+		fuse_ino_t newparent, const char *newname)
+
