f0707a358cc45efba6841845a4f7f27ef11283c4
shaobingyang <shaobingyang@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-03 05:56:36 +0000

    ADD hsx_fuse_access and hsx_fuse_create to the file
    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8301 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- include/hsx_fuse.h
+++ include/hsx_fuse.h
@@ -75,4 +75,39 @@ extern void  (*hsx_fuse_readlink) (fuse_req_t  req,fuse_ino_t ino);
 **/
 extern void (*hsx_fuse_symlink)(fuse_req_t req, const char *link,                      
                                   fuse_ino_t  parent,const char *name);
+
+
+/**
+* hsx_fuse_access
+*
+* To request whether or not you have the permission to perform an operation.
+*
+* Valid replies:
+*   fuse_reply_err
+*
+* @param req request handle
+* @param ino the inode number where you want to perform an operation
+* @param mask the permission you want to request
+**/
+extern void hsx_fuse_access(fuse_req_t req, fuse_ino_t ino, int mask);
+
+
+/**
+* hsx_fuse_create
+*
+* To create a new regular file.
+*
+* Valid replies:
+*		fuse_repay_create
+*   fuse_reply_err
+*
+* @param req request handle
+* @param parent the inode number of the parent directory where 
+*								you want to create a new regular file
+* @param name the name of the file you want to create
+*	@param mode the access mode of the file
+* @param fi	the fuse_file_info which identify the way to create the new file
+**/
+void hsx_fuse_create(fuse_req_t req, fuse_ino_t parent, const char *name,
+							mode_t mode, struct fuse_file_info *fi);                              
 #endif
