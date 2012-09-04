4ad618fc4e00767b1c14ea03a0f39097e0e06bf4
xuehaitao <xuehaitao@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-04 00:48:32 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8312 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- include/hsi_nfs3.h
+++ include/hsi_nfs3.h
@@ -84,7 +84,7 @@ extern  int  hsi_nfs3_readlink( struct hsfs_inode *hi,char *nfs_link);
 *
 * @return error number
 **/
-extern int  hsi_nfs3_symlink(struct hsfs_inode *parent,struct hsfs_inode *new,const char *nfs_link ,const char *nfs_name);
+extern int  hsi_nfs3_symlink(struct hsfs_inode *parent,struct hsfs_inode **new,const char *nfs_link ,const char *nfs_name);
 
 /**
 * File read 
