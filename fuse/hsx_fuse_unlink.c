a2bd18f47f3d614ee036b390403da6e0cde0901f
yanhuan <yanhuan@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-05 10:40:36 +0000

    add hsx_fuse_unlink.c
    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8361 91053cd5-a235-4db3-b8ea-753d4ab5bda5

---
+++ fuse/hsx_fuse_unlink.c
@@ -0,0 +1,42 @@
+#include "hsx_fuse.h"
+
+extern void hsx_fuse_unlink(fuse_req_t req, fuse_ino_t parent, const char *name)
+{
+	int err = 0;
+	if (name == NULL) {
+		ERR("%s name to remove is NULL\n", progname);
+		err = ENOENT;
+		goto out;
+	}
+	else if (!(strcmp(name, ""))) {
+		ERR("%s name to remove is empty\n", progname);
+		err = ENOENT;
+		goto out;
+	}
+	else if (strlen(name) > MAXNAMELEN) {
+		ERR("%s name to remove is too long\n", progname);
+		err = ENAMETOOLONG;
+		goto out;
+	}
+
+	struct hsfs_super *hs = NULL;
+	struct hsfs_inode *hi = NULL;
+	if (!(hs = fuse_req_userdata(req))) {
+		ERR("%s get user data failed\n", progname);
+		err = EIO;
+		goto out;
+	}
+	if (!(hi = hsx_fuse_iget(hs, parent))) {
+		ERR("%s get hsfs inode failed\n", progname);
+		err = EIO;
+		goto out;
+	}
+
+	if((err = hsi_nfs3_remove(hi, name))) {
+		goto out;
+	}
+out:
+	/* remove ino <=> fh? */
+	fuse_reply_err(req, err);
+}
+
