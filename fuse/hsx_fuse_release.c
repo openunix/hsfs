a960548890c8ecb62220d4d123abc988179c5878
liuyoujin <liuyoujin@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-06 07:26:03 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8395 91053cd5-a235-4db3-b8ea-753d4ab5bda5

---
+++ fuse/hsx_fuse_release.c
@@ -0,0 +1,10 @@
+#include <hsx_fuse.h>
+#include <errno.h>
+#include <stdio.h>
+extern void hsx_fuse_release (fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
+{
+	if (fi->fh != NULL){
+		free (fi->fh);
+	}
+	fuse_reply_open(req, 0);
+}
