2a182c07cff63c7d3275e52c6bee067f3ef8a561
liyangzhao <liyangzhao@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-06 12:04:11 +0000

    add test main() entry point
    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8415 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsi_nfs3_read.c
+++ fuse/hsi_nfs3_read.c
@@ -1,8 +1,10 @@
 /*hsi_nfs3_read.c*/
 
-#include "hsfs.h"
+#include <errno.h>
 #include "hsi_nfs3.h"
 #include "log.h"
+
+#define HSFS_TEST
 
 int hsi_nfs3_read(struct hsfs_rw_info* rinfo)
 {
@@ -32,9 +34,9 @@ int hsi_nfs3_read(struct hsfs_rw_info* rinfo)
 		goto out;
 	}
 
-	err = 5;//hsi_nfs3_stat_to_errno(res.status);
+	err = 0;//hsi_nfs3_stat_to_errno(res.status);
 	if(err){
-		ERR("hsi_nfs3_write failure: %x", err);
+		ERR("hsi_nfs3_read failure: %x", err);
 		goto out;
 	}	
 
@@ -50,3 +52,77 @@ out:
 	return err;
 }
 
+#ifdef HSFS_TEST
+int main(int argc, char *argv[])
+{
+	struct hsfs_rw_info rinfo;
+	struct hsfs_inode inode;
+	struct hsfs_super sblock;
+	char *svraddr = NULL;
+	char *fpath = NULL;
+	char *mpoint = NULL;
+	CLIENT *clntp = NULL;
+	size_t fhlen = 0 ;
+	unsigned char *fhvalp = NULL;
+	int err = 0;
+	int i = 0;
+	
+	if(argc != 4){
+		ERR("USEAGE: hsi_nfs3_read  SERVER_IP   FILEPATH	MOUNTPOINT.\n");
+		ERR("EXAMPLE: ./hsi_nfs3_read	10.10.99.120	"
+			"/nfsXport/a/file	/mnt/hsfs.\n");
+		err = EINVAL;
+		goto out;
+	}
+	
+	svraddr = argv[1];
+	fpath = argv[2];
+	mpoint = argv[3];
+	clntp = clnt_create(svraddr, NFS_PROGRAM, NFS_V3, "TCP");
+	if(NULL == clntp) {
+		ERR("Create handle to RPC server (%s, %u, %u) failed\n",
+			svraddr, NFS_PROGRAM, NFS_V3);
+		err = EREMOTEIO;
+		goto out;
+	}
+
+	err = map_path_to_nfs3fh(svraddr, fpath, &fhlen, &fhvalp);
+	if(err)
+	{
+		ERR("map_path_to_nfs3fh error: %s.\n", strerror(err));
+		err = ENXIO;		
+		goto out;
+	}
+	
+	memset(&inode, 0, sizeof(struct hsfs_inode));
+	memset(&rinfo, 0, sizeof(struct hsfs_rw_info));
+	memset(&sblock, 0, sizeof(struct hsfs_super));
+	inode.sb = &sblock;
+	rinfo.inode = &inode;
+	
+	sblock.timeo = 1200;//120sec
+	sblock.clntp = clntp;
+	inode.fh.data.data_len = fhlen;
+	inode.fh.data.data_val = fhvalp;
+	
+#define TEST_FILE_SIZE		(100 * 0x100000) //100MB
+#define TEST_READ_MAX_SIZE 	0x10000 //64KB
+	rinfo.rw_off = 0;
+	rinfo.rw_size = TEST_READ_MAX_SIZE;
+
+	for(i = 0; i < TEST_FILE_SIZE; ){
+		rinfo.rw_off += rinfo.ret_count;
+		err = hsi_nfs3_read(&rinfo);
+		if(err)
+			break;
+		
+		i += rinfo.ret_count;		
+		if(rinfo.eof)
+			break;		
+	}
+
+out:
+	return err;
+}
+
+#endif /*HSFS_TEST*/
--- fuse/hsi_nfs3_write.c
+++ fuse/hsi_nfs3_write.c
@@ -1,8 +1,10 @@
 /*hsi_nfs3_write.c*/
 
-#include "hsfs.h"
+#include <errno.h>
 #include "hsi_nfs3.h"
 #include "log.h"
+
+#define HSFS_TEST
 
 int hsi_nfs3_write(struct hsfs_rw_info* winfo)
 {
@@ -33,7 +35,7 @@ int hsi_nfs3_write(struct hsfs_rw_info* winfo)
 		goto out;
 	}
 
-	err = 5;//hsi_nfs3_stat_to_errno(res.status);
+	err = 0;//hsi_nfs3_stat_to_errno(res.status);
 	if(err){
 		ERR("hsi_nfs3_write failure: %x", err);
 		goto out;
@@ -46,3 +48,85 @@ int hsi_nfs3_write(struct hsfs_rw_info* winfo)
 out:
 	return err;
 }
+
+
+#ifdef HSFS_TEST
+int main(int argc, char *argv[])
+{
+	struct hsfs_rw_info winfo;
+	struct hsfs_inode inode;
+	struct hsfs_super sblock;
+	char *svraddr = NULL;
+	char *fpath = NULL;
+	char *mpoint = NULL;
+	char *buffer = NULL;
+	CLIENT *clntp = NULL;
+	size_t fhlen = 0 ;
+	unsigned char *fhvalp = NULL;
+	int err = 0;
+	int i = 0;
+	
+	if(argc != 4){
+		ERR("USEAGE: hsi_nfs3_write  SERVER_IP   FILEPATH	MOUNTPOINT.\n");
+		ERR("EXAMPLE: ./hsi_nfs3_write	10.10.99.120	"
+			"/nfsXport/a/file	/mnt/hsfs.\n");
+		err = EINVAL;
+		goto out;
+	}
+	
+	svraddr = argv[1];
+	fpath = argv[2];
+	mpoint = argv[3];
+	clntp = clnt_create(svraddr, NFS_PROGRAM, NFS_V3, "TCP");
+	if(NULL == clntp){
+		ERR("Create handle to RPC server (%s, %u, %u) failed\n",
+			svraddr, NFS_PROGRAM, NFS_V3);
+		err = EREMOTEIO;
+		goto out;
+	}
+
+	err = map_path_to_nfs3fh(svraddr, fpath, &fhlen, &fhvalp);
+	if(err){
+		ERR("map_path_to_nfs3fh error: %s.\n", strerror(err));
+		err = ENXIO;		
+		goto out;
+	}
+	
+	memset(&inode, 0, sizeof(struct hsfs_inode));
+	memset(&winfo, 0, sizeof(struct hsfs_rw_info));
+	memset(&sblock, 0, sizeof(struct hsfs_super));
+	inode.sb = &sblock;
+	winfo.inode = &inode;
+	
+	sblock.timeo = 1200;//120sec
+	sblock.clntp = clntp;
+	inode.fh.data.data_len = fhlen;
+	inode.fh.data.data_val = fhvalp;
+	
+#define TEST_FILE_SIZE		(100 * 0x100000) //100MB
+#define TEST_WRITE_MAX_SIZE 	0x10000 //64KB
+	winfo.rw_off = 0;
+	winfo.rw_size = TEST_WRITE_MAX_SIZE;
+	buffer = (char *) malloc(TEST_WRITE_MAX_SIZE);
+	if( NULL == buffer){
+		ERR("insufficient memory.\n");
+		err = ENOMEM;
+		goto out;
+	}
+	memset(buffer, '0', TEST_WRITE_MAX_SIZE);
+	for(i = 0; i < TEST_FILE_SIZE; ){
+		winfo.rw_off += winfo.ret_count;
+		err = hsi_nfs3_write(&winfo);
+		if(err)
+			break;
+		
+		i += winfo.ret_count;
+	}
+out:
+	if(NULL != buffer)
+		free(buffer);
+
+	return err;
+}
+
+#endif /*HSFS_TEST*/
