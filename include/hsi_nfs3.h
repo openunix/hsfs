4d616f414e2aa7ba5ba2274c1169bbc35355b8f2
huyuwei <huyuwei@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-06 01:54:54 +0000

    change the input hsfs_inode to hi_parent in hsi_nfs3.h
    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8375 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- include/hsi_nfs3.h
+++ include/hsi_nfs3.h
@@ -19,7 +19,7 @@
  * */
 extern int hsi_nfs3_mkdir(struct hsfs_inode *parent, struct hsfs_inode **new, char *name, mode_t mode);
 
-extern int hsi_nfs3_rmdir(struct hsfs_inode *hsfs_inode, char *name);
+extern int hsi_nfs3_rmdir(struct hsfs_inode *hi_parent, char *name);
 
 /**
 * Remove a file
