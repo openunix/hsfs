90a4c63a82e0e5038732b0a7bc761881b8a26152
zhaoyan <zhaoyan@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-06 02:10:34 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8377 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- include/hsfs.h
+++ include/hsfs.h
@@ -1,18 +1,18 @@
 
 #ifndef __HSFS_H__
-#define __HSFS_H__ 1
+#define __HSFS_H__
 
 #ifndef FUSE_USE_VERSION
 #define FUSE_USE_VERSION 26
 #endif
 
 #include <sys/stat.h>
-#include <fuse/fuse_lowlevel.h>
-#include <rpc/rpc.h>
-
+#include <sys/types.h>
+#include <time.h>
 #include "nfs3.h"
 
-#define HSFS_TYPE	"hsfs"
+
+#define HSFS_TYPE "hsfs"
 
 extern char *progname;
 extern int nomtab;
@@ -36,108 +36,108 @@ extern int fg;
 #define HSFS_MAX_READDIR_PAGES 8
 
 struct hsfs_fh {
-        unsigned short          size;
-        unsigned char           data[64];
+  unsigned short  size;
+  unsigned char   data[64];
 };
 
 struct hsfs_inode
 {
-        uint64_t		ino;
-        unsigned long 		generation;
-        nfs_fh3			fh;
-        unsigned long		nlookup;
-        fattr3			attr;
-        struct hsfs_super	*sb;
-        struct hsfs_inode 	*next;
+  uint64_t          ino;
+  unsigned long     generation;
+  nfs_fh3           fh;
+  unsigned long     nlookup;
+  fattr3            attr;
+  struct hsfs_super *sb;
+  struct hsfs_inode *next;
 };
 
 struct  hsfs_table
 {
-        struct hsfs_inode	**array;
-        size_t			use;
-        size_t			size;
+  struct hsfs_inode  **array;
+  size_t  use;
+  size_t  size;
 };
 
 struct hsfs_super {
-	CLIENT			*clntp;
-	int			flags;
-	/* rsize/wrise, filled up at hsx_fuse_init 
-	  * for read/write
-	  */
-	int			rsize;
-	int			wsize;
-	/* For all clnt_call timeout,
-	  * as deciseconds (tenths of a second)
-	  */
-	int			timeo;
-	int			retrans;
-	int			acregmin;
-	int			acregmax;
-	int			acdirmin;
-	int			acdirmax;
-	struct sockaddr_in	addr;
-	/* namelen, filled up at hsx_fuse_init */
-	int			namlen;
-	/* Readdir size */
-	int			dtsize;
-	unsigned int		bsize;
-	unsigned char		bsize_bits;
-	struct hsfs_inode	*root;
-	struct hsfs_table       hsfs_fuse_ht;
-	/* Copy from FSINFO result directly, be careful */
-	uint32_t		rtmax;
-	uint32_t		rtpref;
-	uint32_t		rtmult;
-	uint32_t		wtmax;
-	uint32_t		wtpref;
-	uint32_t		wtmult;
-	uint32_t		dtpref;
-	uint64_t		maxfilesize;
-	nfstime3		time_delta;
-	uint32_t		properties;
-
-	/* Copy from FSSTAT result directly, be careful */
-	uint64_t		tbytes;
-	uint64_t		fbytes;
-	uint64_t		abytes;
-	uint64_t		tfiles;
-	uint64_t		ffiles;
-	uint64_t		afiles;
+  CLIENT *clntp;
+  int    flags;
+  /* rsize/wrise, filled up at hsx_fuse_init 
+   * for read/write
+   */
+  int    rsize;
+  int	 wsize;
+  /* For all clnt_call timeout,
+   * as deciseconds (tenths of a second)
+   */
+  int	 timeo;
+  int    retrans;
+  int	 acregmin;
+  int	 acregmax;
+  int	 acdirmin;
+  int	 acdirmax;
+  struct sockaddr_in	addr;
+  /* namelen, filled up at hsx_fuse_init */
+  int	 namlen;
+  /* Readdir size */
+  int	 dtsize;
+  unsigned int	    bsize;
+  unsigned char	    bsize_bits;
+  struct hsfs_inode *root;
+  struct hsfs_table hsfs_fuse_ht;
+  /* Copy from FSINFO result directly, be careful */
+  uint32_t	    rtmax;
+  uint32_t	    rtpref;
+  uint32_t	    rtmult;
+  uint32_t	    wtmax;
+  uint32_t	    wtpref;
+  uint32_t	    wtmult;
+  uint32_t	    dtpref;
+  uint64_t	    maxfilesize;
+  nfstime3	    time_delta;
+  uint32_t	    properties;
+  
+  /* Copy from FSSTAT result directly, be careful */
+  uint64_t	    tbytes;
+  uint64_t	    fbytes;
+  uint64_t	    abytes;
+  uint64_t	    tfiles;
+  uint64_t	    ffiles;
+  uint64_t	    afiles;
 };
 
 #define HSFS_FILE_SYNC  2
 #define HSFS_UNSTABLE	0
 
 struct hsfs_rw_info {
-	struct hsfs_inode	*inode;
-	size_t			rw_size;//read or write size
-	off_t			rw_off;//read or write offset
-	size_t			ret_count;//result of operate
-	int			eof;//end of file
-	int			stable;//direct io flag
-	struct {
-				 size_t data_len;
-				 char *data_val;
-			} data;//data buffer
+  struct hsfs_inode	*inode;
+  size_t	  	rw_size;//read or write size
+  off_t			rw_off;//read or write offset
+  size_t		ret_count;//result of operate
+  int			eof;//end of file
+  int			stable;//direct io flag
+  struct {
+    size_t data_len;
+    char *data_val;
+  } data;//data buffer
 };
 
 struct hsfs_sattr{
-	int	valid;
-	mode_t	mode;
-	uid_t	uid;
-	gid_t	gid;
-	off_t	size;
-	struct timespec	atime;
-	struct timespec	mtime;
+  int    valid;
+  mode_t mode;
+  uid_t	 uid;
+  gid_t	 gid;
+  off_t	 size;
+  struct timespec atime;
+  struct timespec mtime;
 }hsfs_sattr_t;
 
 struct hsfs_readdir_ctx{
-	off_t	off;
-	char	*name;
-	char	cookieverf[NFS3_COOKIEVERFSIZE];
-	struct stat	stbuf;
-	struct hsfs_readdir_ctx *next;
- };
+  off_t	off;
+  char	*name;
+  char	cookieverf[NFS3_COOKIEVERFSIZE];
+  struct stat	stbuf;
+  struct hsfs_readdir_ctx *next;
+};
 
 /* table function declaration */
 extern	int hsx_fuse_init(struct hsfs_super *sb);
--- include/hsx_fuse.h
+++ include/hsx_fuse.h
@@ -20,20 +20,20 @@ extern void hsx_fuse_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off
 extern void hsx_fuse_write(fuse_req_t req, fuse_ino_t ino, const char *buf, size_t size, off_t off, struct fuse_file_info *fi);
 
 /**
-* Remove a file
-*
-* If the file's inode's lookup count is non-zero, the file
-* system is expected to postpone any removal of the inode
-* until the lookup count reaches zero (see description of the
-* forget function).
-*
-* Valid replies:
-*   fuse_reply_err
-*
-* @param req request handle
-* @param parent inode number of the parent directory
-* @param name to remove
-**/
+ * Remove a file
+ *
+ * If the file's inode's lookup count is non-zero, the file
+ * system is expected to postpone any removal of the inode
+ * until the lookup count reaches zero (see description of the
+ * forget function).
+ *
+ * Valid replies:
+ *   fuse_reply_err
+ *
+ * @param req request handle
+ * @param parent inode number of the parent directory
+ * @param name to remove
+ **/
 extern void hsx_fuse_unlink(fuse_req_t req, fuse_ino_t parent, const char *name);
 
 /** 
@@ -55,67 +55,67 @@ extern void hsx_fuse_unlink(fuse_req_t req, fuse_ino_t parent, const char *name)
  * @param newname new name
  **/
 extern void hsx_fuse_rename(fuse_req_t req, fuse_ino_t parent, const char *name, 
-		fuse_ino_t newparent, const char *newname);
+			    fuse_ino_t newparent, const char *newname);
 
 /**
-* read the content of the symlink
-* Valid replies:
-*   fuse_reply_readlink
-*   fuse_reply_err
-*
-* @param req request handle
-* @param ino the inode number
-**/
+ * read the content of the symlink
+ * Valid replies:
+ *   fuse_reply_readlink
+ *   fuse_reply_err
+ *
+ * @param req request handle
+ * @param ino the inode number
+ **/
 extern void hsx_fuse_readlink(fuse_req_t req, fuse_ino_t ino);
 
 /**
-* Create a symbolic link
-* Valid replies:
-*   fuse_reply_entry
-*   fuse_reply_err
-*
-* @param req request handle
-* @param link the contents of the symbolic link
-* @param parent inode number of the parent directory
-* @param name to create
-**/
+ * Create a symbolic link
+ * Valid replies:
+ *   fuse_reply_entry
+ *   fuse_reply_err
+ *
+ * @param req request handle
+ * @param link the contents of the symbolic link
+ * @param parent inode number of the parent directory
+ * @param name to create
+ **/
 extern void hsx_fuse_symlink(fuse_req_t req, const char *link,                      
-                                  fuse_ino_t parent, const char *name);
+			     fuse_ino_t parent, const char *name);
 
 
 /**
-* hsx_fuse_access
-*
-* To request whether or not you have the permission to perform an operation.
-*
-* Valid replies:
-*   fuse_reply_err
-*
-* @param req request handle
-* @param ino the inode number where you want to perform an operation
-* @param mask the permission you want to request
-**/
+ * hsx_fuse_access
+ *
+ * To request whether or not you have the permission to perform an operation.
+ *
+ * Valid replies:
+ *   fuse_reply_err
+ *
+ * @param req request handle
+ * @param ino the inode number where you want to perform an operation
+ * @param mask the permission you want to request
+ **/
 extern void hsx_fuse_access(fuse_req_t req, fuse_ino_t ino, int mask);
 
 
 /**
-* hsx_fuse_create
-*
-* To create a new regular file.
-*
-* Valid replies:
-*		fuse_repay_create
-*   fuse_reply_err
-*
-* @param req request handle
-* @param parent the inode number of the parent directory where 
-*								you want to create a new regular file
-* @param name the name of the file you want to create
-* @param mode the access mode of the file
-* @param fi	the fuse_file_info which identify the way to create the new file
-**/
+ * hsx_fuse_create
+ *
+ * To create a new regular file.
+ *
+ * Valid replies:
+ *		fuse_repay_create
+ *   fuse_reply_err
+ *
+ * @param req request handle
+ * @param parent the inode number of the parent directory where 
+ *								you want to create a new regular file
+ * @param name the name of the file you want to create
+ * @param mode the access mode of the file
+ * @param fi	the fuse_file_info which identify the way to create the new file
+ **/
 extern void hsx_fuse_create(fuse_req_t req, fuse_ino_t parent, const char *name,
-							mode_t mode, struct fuse_file_info *fi);                              
+			    mode_t mode, struct fuse_file_info *fi);                              
 
 /**
  * hsx_fuse_mknod
@@ -149,7 +149,7 @@ extern	void hsx_fuse_mknod(fuse_req_t	req, fuse_ino_t	parent,	const char *name, 
  * @param  newparent	inode number of the new parent directory
  * @param  newname	new name to create
  * */
-extern	void hsx_fuse_link(fuse_req_t	req, fuse_ino_t ino,	fuse_ino_t newparent, const char *newname);
+extern	void hsx_fuse_link(fuse_req_t req, fuse_ino_t ino, fuse_ino_t newparent, const char *newname);
 
 /*hsx_fuse_lookup  and  hsx_fuse_forget declaration */
 
@@ -165,20 +165,19 @@ extern void hsx_fuse_open (fuse_req_t req, fuse_ino_t ino, struct fuse_file_info
 extern void hsx_fuse_release (fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi);
 
 /**
-* The two functions used to get and set file attributions are defined below, 
-* which use the fues_reply_err or fues_reply_attr to return values, showed 
-* as follows.
-*
-* return functions:
-* int fuse_reply_err(fuse_req_t req, int err);
-* int fuse_reply_attr(fuse_req_t req, const struct stat *attr, 
-* double attr_timeout);
-* Date:2012-09-04
-*/
+ * The two functions used to get and set file attributions are defined below, 
+ * which use the fues_reply_err or fues_reply_attr to return values, showed 
+ * as follows.
+ *
+ * return functions:
+ * int fuse_reply_err(fuse_req_t req, int err);
+ * int fuse_reply_attr(fuse_req_t req, const struct stat *attr, 
+ * double attr_timeout);
+ */
 extern void hsx_fuse_getattr(fuse_req_t req, fuse_ino_t ino, 
-							struct fuse_file_info *fi);
+			     struct fuse_file_info *fi);
 extern void hsx_fuse_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
-							int to_set, struct fuse_file_info *fi);
+			     int to_set, struct fuse_file_info *fi);
 /**
  * hsx_fuse_statfs
  * Get the filesystem statistics
@@ -186,32 +185,32 @@ extern void hsx_fuse_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
  * fuse_reply_statfs
  * fuse_reply_err
  **/
- extern void hsx_fuse_statfs(fuse_req_t req, fuse_ino_t ino);
+extern void hsx_fuse_statfs(fuse_req_t req, fuse_ino_t ino);
 
- /**
-* read the content of the directory
-* Valid replies:
-*   fuse_reply_buf
-*   fuse_reply_err
-*
-* @param req         request handle
-* @param ino         the inode number
-* @param size        the maximum number of bytes to send
-* @param off         the offset to continue reading the directory
-* @param fi          the file information
-**/
+/**
+ * read the content of the directory
+ * Valid replies:
+ *   fuse_reply_buf
+ *   fuse_reply_err
+ *
+ * @param req         request handle
+ * @param ino         the inode number
+ * @param size        the maximum number of bytes to send
+ * @param off         the offset to continue reading the directory
+ * @param fi          the file information
+ **/
 extern void hsx_fuse_readdir(fuse_req_t req,  fuse_ino_t ino,  size_t size,  off_t off,  struct fuse_file_info  *fi);
 
 /**
-* open a directory
-* Valid replies:
-*   fuse_reply_open
-*   fuse_reply_err
-*
-* @param req         request handle
-* @param ino         the inode number
-* @param fi          the file information
-**/
+ * open a directory
+ * Valid replies:
+ *   fuse_reply_open
+ *   fuse_reply_err
+ *
+ * @param req         request handle
+ * @param ino         the inode number
+ * @param fi          the file information
+ **/
 extern void hsx_fuse_opendir(fuse_req_t req,  fuse_ino_t ino,  struct fuse_file_info  *fi);
 
 #endif
