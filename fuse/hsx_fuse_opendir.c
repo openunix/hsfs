e1833f8f384349a4239aef0c69765047dee4aab9
huangyongsheng <huangyongsheng@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-07 08:03:23 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8453 91053cd5-a235-4db3-b8ea-753d4ab5bda5

---
+++ fuse/hsx_fuse_opendir.c
@@ -0,0 +1,25 @@
+/*
+ *hsx_fuse_readdir.c
+ */
+
+#include <fuse/fuse_lowlevel.h>
+#include "hsi_nfs3.h"
+
+void hsx_fuse_opendir(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
+{
+	struct hsfs_readdir_ctx *hrc = NULL;
+	int err = 0;
+
+	hrc = (struct hsfs_readdir_ctx*)malloc(sizeof(struct hsfs_readdir_ctx));
+	if (hrc == NULL)
+		printf("No menory: hrc.");
+	
+	memset(hrc, 0, sizeof(struct hsfs_readdir_ctx));
+	fi->fh = hrc;
+	fuse_reply_open(req, fi);
+
+out:	
+	
+	return;
+}
+
