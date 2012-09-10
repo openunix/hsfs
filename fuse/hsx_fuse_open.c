a1d2d582a9f7b9a4972add9e68da06b7221dc426
liuyoujin <liuyoujin@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-10 06:50:52 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8509 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_open.c
+++ fuse/hsx_fuse_open.c
@@ -12,7 +12,7 @@
 #define FI_FH_LEN    10
 extern void hsx_fuse_open (fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 {
-DEBUG_IN ("INODE:%ul",(unsigned long)ino);
+DEBUG_IN ("%s","Enter hsx_fuse_open");
 	uint64_t fh =(uint64_t) malloc (FI_FH_LEN);
 	if (fh){
 		fuse_reply_err(req, -ENOMEM);
@@ -22,6 +22,6 @@ DEBUG_IN ("INODE:%ul",(unsigned long)ino);
 		fi->fh = fh;
 		fuse_reply_open(req, fi);
 	}
-DEBUG_OUT("INODE:%ul",(unsigned long)ino);	
+DEBUG_OUT("%s","Exit hsx_fuse_open");	
 }
 
--- fuse/hsx_fuse_release.c
+++ fuse/hsx_fuse_release.c
@@ -9,10 +9,10 @@
 #include "log.h"
 extern void hsx_fuse_release (fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 {
-DEBUG_IN ("inode:%ul",(unsigned long)ino);
+DEBUG_IN ("%s","Enter hsx_fuse_release");
 	if (fi->fh != 0){
 		free ((void *)fi->fh);
 	}
 	fuse_reply_err(req, 0);
-DEBUG_OUT("inode:%ul",(unsigned long)ino);
+DEBUG_OUT("%s","Exit hsx_fuse_release");
 }
