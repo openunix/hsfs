ff6dbcbae3ba806e4678d30c8b63365da5d93339
liuyoujin <liuyoujin@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-10 05:42:40 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8502 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_release.c
+++ fuse/hsx_fuse_release.c
@@ -5,10 +5,13 @@
 #include <errno.h>
 #include <stdio.h>
 #include <fuse/fuse_lowlevel.h>
+#include "log.h"
 extern void hsx_fuse_release (fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 {
+DEBUG_IN ("INODE",ino);
 	if (fi->fh != NULL){
 		free (fi->fh);
 	}
 	fuse_reply_err(req, 0);
+DEBUG_OUT("INODE",ino);
 }
