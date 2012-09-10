5ce3cf8600dd1a5b7f52ba39dd4872e2f1e08939
huyuwei <huyuwei@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-10 07:31:01 +0000

    fix a bug in hsx_fuse_mkdir.c
    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8520 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_mkdir.c
+++ fuse/hsx_fuse_mkdir.c
@@ -14,27 +14,25 @@ void hsx_fuse_mkdir(fuse_req_t req, fuse_ino_t parent, const char *name,
 	int err = 0;
 	struct hsfs_inode *hi_parent;
 	struct hsfs_inode *new;
-	struct hsfs_super *sp;
+	//struct hsfs_super *sp;
 	struct fuse_entry_param *e;
 	char *dirname = name;
-	
 	hi_parent = (struct hsfs_inode *) malloc (sizeof(struct hsfs_inode));
 	new = (struct hsfs_inode *) malloc (sizeof(struct hsfs_inode));
-	sp = (struct hsfs_super *) malloc (sizeof(struct hsfs_super));
+	hi_parent->sb = (struct hsfs_super *) malloc (sizeof(struct hsfs_super));
 	e = (struct fuse_entry_param *) malloc (sizeof(struct fuse_entry_param));
-	
 	memset(e, 0, sizeof(struct fuse_entry_param));
-	memset(sp, 0, sizeof(struct hsfs_super));
+	memset(hi_parent->sb, 0, sizeof(struct hsfs_super));
 	memset(new, 0, sizeof(struct hsfs_inode));
 	memset(hi_parent, 0, sizeof(struct hsfs_inode));
 
-	sp = fuse_req_userdata(req);
-	hi_parent = hsx_fuse_iget(sp, parent);
+	hi_parent->sb = fuse_req_userdata(req);
+	hi_parent = hsx_fuse_iget(hi_parent->sb, parent);
 	err = hsi_nfs3_mkdir(hi_parent, &new, dirname, mode);
-	
 	if(0 != err )
 	{
 		fuse_reply_err(req, err);
+		INFO("A %d",err);
 		goto out;
 	}
 	else
@@ -44,10 +42,11 @@ void hsx_fuse_mkdir(fuse_req_t req, fuse_ino_t parent, const char *name,
 		goto out;
 	}
 out:
-	free(hi_parent);
+	INFO("I 1--! was here in hsx_fuse_mkdir.c");
 	free(new);
-	free(sp);
+	INFO("I 2--! was here in hsx_fuse_mkdir.c");
 	free(e);
+	INFO("I 4--! was here in hsx_fuse_mkdir.c");
 	return;
 };
 
