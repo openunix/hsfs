df59675632bc6f66af95293023ccae23361db89c
xuehaitao <xuehaitao@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-12 11:03:27 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8740 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_readlink.c
+++ fuse/hsx_fuse_readlink.c
@@ -6,6 +6,7 @@
 #include "hsi_nfs3.h"
 #include "hsx_fuse.h"
 #include "log.h"
+#include <errno.h>
 
 void hsx_fuse_readlink(fuse_req_t req, fuse_ino_t ino)
 {
@@ -14,20 +15,26 @@ void hsx_fuse_readlink(fuse_req_t req, fuse_ino_t ino)
 	struct hsfs_inode *hi = NULL;
 	struct hsfs_super *hi_sb = NULL;
 	char *link = NULL;
+	DEBUG_IN("%s\n","THE HSX_FUSE_READLINK.");
 
 	hi_sb = fuse_req_userdata(req);
+	if(!hi_sb){
+		ERR("%s gets inode->sb fails \n",progname);
+		err=ENOENT;
+		goto out;
+	}
 	hi = hsx_fuse_iget(hi_sb, ino);
-	DEBUG_IN("%s\n","THE HSX_FUSE_READLINK.");
+	if(!hi){
+		ERR("%s gets inode fails \n",progname);
+		err=ENOENT;
+		goto out;
+	}
 	st = hsi_nfs3_readlink(hi,&link);
 	if(st != 0){
 		err = st;
 		goto out;
 	}
-	st = fuse_reply_readlink(req, link);
-	if(st != 0){
-		err = st;
-		goto out;
-	}
+	fuse_reply_readlink(req, link);
 
 out:
 	free(link);
