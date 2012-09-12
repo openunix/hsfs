f36c33292ac37bbfabc274da4549d9c3eab0d109
huangyongsheng <huangyongsheng@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-12 11:01:14 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8738 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_opendir.c
+++ fuse/hsx_fuse_opendir.c
@@ -1,26 +1,26 @@
 /*
  *hsx_fuse_readdir.c
  */
-
+#include <error.h>
 #include <fuse/fuse_lowlevel.h>
 #include "hsi_nfs3.h"
-
 void hsx_fuse_opendir(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 {
 	struct hsfs_readdir_ctx *hrc = NULL;
+	int err = 0;
 
 	DEBUG_IN("%s.","hsx_fuse_opendir");
 	hrc = (struct hsfs_readdir_ctx*)malloc(sizeof(struct hsfs_readdir_ctx));
 	if (hrc == NULL)
-	{
+	{	err = ENOMEM;
 		ERR("opendir_hrc menory leak.");
+		fuse_reply_err(req, err);
 		goto out;
 	}
 
 	memset(hrc, 0, sizeof(struct hsfs_readdir_ctx));
 	fi->fh = (size_t)hrc;
 	fuse_reply_open(req, fi);
-	free(hrc);
 
 out:	
 	DEBUG_OUT("%s.","hsx_fuse_opendir");
