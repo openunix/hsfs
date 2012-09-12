ca2b65593428423a665db26c26c592d4c3635380
liuyoujin <liuyoujin@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-12 10:33:24 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8723 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_statfs.c
+++ fuse/hsx_fuse_statfs.c
@@ -6,20 +6,21 @@
 #include <sys/errno.h>
 #include "hsi_nfs3.h"
 #include "hsx_fuse.h"
-extern void hsx_fuse_statfs(fuse_req_t req, fuse_ino_t ino)
+
+void hsx_fuse_statfs(fuse_req_t req, fuse_ino_t ino)
 {
 	struct statvfs stbuf;
-        struct hsfs_super *super = NULL;
+	struct hsfs_super *super = NULL;
 	struct hsfs_inode *root = NULL;
 	struct hsfs_super *sp = NULL;
 	int err=0;
 	DEBUG_IN(" err:%d", err);
 
-        super = fuse_req_userdata(req);
+	super = fuse_req_userdata(req);
 	root = super->root;
 	sp = root->sb;
 	err = hsi_nfs3_statfs(root);
-        if (err){
+	if (err){
 		fuse_reply_err (req, err);
 		goto out;
 	}
