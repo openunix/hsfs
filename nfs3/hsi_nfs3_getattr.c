9c18a891506c514fc6c2b464c2f768a47b46830d
zhaoyan <zhaoyan@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-12 07:47:25 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8686 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- nfs3/hsi_nfs3_getattr.c
+++ nfs3/hsi_nfs3_getattr.c
@@ -4,19 +4,23 @@ int hsi_nfs3_getattr(struct hsfs_inode *inode)
 {
 	int err = 0;
 	nfs_fh3 fh;
-	CLIENT *clntp;
-	struct timeval to = {120, 0};
+	CLIENT *clntp = NULL;
+	struct timeval to;
 	struct getattr3res res;
 	enum clnt_stat st;
+	struct fattr3 *attr = NULL;
 	
 	DEBUG_IN("%s", "Enter hsi_nfs3_getattr().\n");
 
 	clntp= inode->sb->clntp;
+	to.tv_sec = inode->sb->timeo / 10;
+        to.tv_usec = (inode->sb->timeo % 10) * 100000;
 	fh.data.data_len = inode->fh.data.data_len;
 	fh.data.data_val = inode->fh.data.data_val;
 	memset(&res, 0, sizeof(res));
-	st = clnt_call(clntp, 1, (xdrproc_t)xdr_nfs_fh3, (caddr_t)&fh, 
-		       (xdrproc_t)xdr_getattr3res, (caddr_t)&res, to);
+	st = clnt_call(clntp, NFSPROC3_GETATTR, (xdrproc_t)xdr_nfs_fh3, 
+		       (caddr_t)&fh, (xdrproc_t)xdr_getattr3res, (caddr_t)&res,
+		       to);
 	if (st) {
 		err = hsi_rpc_stat_to_errno(clntp);
 		ERR("Call RPC Server failure: %d.\n", err);
@@ -28,25 +32,27 @@ int hsi_nfs3_getattr(struct hsfs_inode *inode)
 		goto out;
 	}
 	/* fill return value to fattr3 attr in struct hsfs_inode */
-	inode->attr.type = res.getattr3res_u.attributes.type;
-	inode->attr.mode = res.getattr3res_u.attributes.mode;
-	inode->attr.nlink = res.getattr3res_u.attributes.nlink;
-	inode->attr.uid = res.getattr3res_u.attributes.uid;
-	inode->attr.gid = res.getattr3res_u.attributes.gid;
-	inode->attr.size = res.getattr3res_u.attributes.size;
-	inode->attr.used = res.getattr3res_u.attributes.used;
-	inode->attr.rdev.major = res.getattr3res_u.attributes.rdev.major;
-	inode->attr.rdev.minor = res.getattr3res_u.attributes.rdev.minor;
-	inode->attr.fsid = res.getattr3res_u.attributes.fsid;
-	inode->attr.fileid = res.getattr3res_u.attributes.fileid;
-	inode->attr.atime.seconds = res.getattr3res_u.attributes.atime.seconds;
-	inode->attr.atime.nseconds = res.getattr3res_u.attributes.atime.nseconds;
-	inode->attr.mtime.seconds = res.getattr3res_u.attributes.mtime.seconds;
-	inode->attr.mtime.nseconds = res.getattr3res_u.attributes.mtime.nseconds;
-	inode->attr.ctime.seconds = res.getattr3res_u.attributes.ctime.seconds;
-	inode->attr.ctime.nseconds = res.getattr3res_u.attributes.ctime.nseconds;
+	attr = &res.getattr3res_u.attributes;
+	inode->attr.type = attr->type;
+	inode->attr.mode = attr->mode;
+	inode->attr.nlink = attr->nlink;
+	inode->attr.uid = attr->uid;
+	inode->attr.gid = attr->gid;
+	inode->attr.size = attr->size;
+	inode->attr.used = attr->used;
+	inode->attr.rdev.major = attr->rdev.major;
+	inode->attr.rdev.minor = attr->rdev.minor;
+	inode->attr.fsid = attr->fsid;
+	inode->attr.fileid = attr->fileid;
+	inode->attr.atime.seconds = attr->atime.seconds;
+	inode->attr.atime.nseconds = attr->atime.nseconds;
+	inode->attr.mtime.seconds = attr->mtime.seconds;
+	inode->attr.mtime.nseconds = attr->mtime.nseconds;
+	inode->attr.ctime.seconds = attr->ctime.seconds;
+	inode->attr.ctime.nseconds = attr->ctime.nseconds;
+	
+	clnt_freeres(clntp, (xdrproc_t)xdr_getattr3res, (char *)&res);
  out:
 	DEBUG_OUT("Leave hsi_nfs3_getattr() with errno %d.\n", err);
-	clnt_freeres(clntp, (xdrproc_t)xdr_getattr3res, (char *)&res);
 	return err;
 }
--- nfs3/hsi_nfs3_setattr.c
+++ nfs3/hsi_nfs3_setattr.c
@@ -1,62 +1,21 @@
 #include <errno.h>
 #include "hsi_nfs3.h"
 
-/* Defined in fuse/fuse_lowlevel.h */
-#ifndef FUSE_SET_ATTR_MODE 
-#define FUSE_SET_ATTR_MODE       (1 << 0)
-#endif
-
-#ifndef FUSE_SET_ATTR_UID
-#define FUSE_SET_ATTR_UID        (1 << 1)
-#endif
-
-#ifndef FUSE_SET_ATTR_GID
-#define FUSE_SET_ATTR_GID        (1 << 2)
-#endif
-
-#ifndef FUSE_SET_ATTR_SIZE
-#define FUSE_SET_ATTR_SIZE       (1 << 3)
-#endif
-
-#ifndef FUSE_SET_ATTR_ATIME
-#define FUSE_SET_ATTR_ATIME      (1 << 4)
-#endif
-
-#ifndef FUSE_SET_ATTR_MTIME
-#define FUSE_SET_ATTR_MTIME      (1 << 5)
-#endif
-
-#ifndef FUSE_SET_ATTR_ATIME_NOW
-#define FUSE_SET_ATTR_ATIME_NOW  (1 << 7)
-#endif
-
-#ifndef FUSE_SET_ATTR_MTIME_NOW
-#define FUSE_SET_ATTR_MTIME_NOW  (1 << 8)
-#endif
-
-/* Custom defined */
-#define S_ISSETMODE(to_set)       ((to_set) & FUSE_SET_ATTR_MODE)
-#define S_ISSETUID(to_set)        ((to_set) & FUSE_SET_ATTR_UID)
-#define S_ISSETGID(to_set)        ((to_set) & FUSE_SET_ATTR_GID)
-#define S_ISSETSIZE(to_set)       ((to_set) & FUSE_SET_ATTR_SIZE)
-#define S_ISSETATIME(to_set)      ((to_set) & FUSE_SET_ATTR_ATIME)
-#define S_ISSETMTIME(to_set)      ((to_set) & FUSE_SET_ATTR_MTIME)
-#define S_ISSETATIMENOW(to_set)   ((to_set) & FUSE_SET_ATTR_ATIME_NOW)
-#define S_ISSETMTIMENOW(to_set)   ((to_set) & FUSE_SET_ATTR_MTIME_NOW)
-
-
 int hsi_nfs3_setattr(struct hsfs_inode *inode, struct hsfs_sattr *attr)
 {
 	int err = 0;
 	CLIENT *clntp = NULL;
-	struct timeval to = {120, 0};
+	struct timeval to;
 	struct setattr3args args;
 	struct wccstat3 res;
 	enum clnt_stat st;
-  
+	struct fattr3 *fattr = NULL;
+	
 	DEBUG_IN("%s", "Enter hsi_nfs3_setattr().\n");
 
 	clntp= inode->sb->clntp;
+	to.tv_sec = inode->sb->timeo / 10;
+	to.tv_usec = (inode->sb->timeo % 10) * 100000;
 	memset(&args, 0, sizeof(args));
 	args.object.data.data_len = inode->fh.data.data_len;
       	args.object.data.data_val = inode->fh.data.data_val;
@@ -112,8 +71,8 @@ int hsi_nfs3_setattr(struct hsfs_inode *inode, struct hsfs_sattr *attr)
 	
 	args.guard.check = FALSE;
 	memset(&res, 0, sizeof(res));
-	st = clnt_call(clntp, 2, (xdrproc_t)xdr_setattr3args, (caddr_t)&args, 
-		       (xdrproc_t)xdr_wccstat3, (caddr_t)&res, to);
+	st = clnt_call(clntp, NFSPROC3_SETATTR, (xdrproc_t)xdr_setattr3args, 
+		(caddr_t)&args, (xdrproc_t)xdr_wccstat3, (caddr_t)&res, to);
 	if (st) {
 		err = hsi_rpc_stat_to_errno(clntp);
 		ERR("Call RPC Server failure: %d.\n", err);
@@ -129,27 +88,28 @@ int hsi_nfs3_setattr(struct hsfs_inode *inode, struct hsfs_sattr *attr)
 		ERR("Try again to set file attributes.\n");
 		goto out;
 	}
-	inode->attr.type = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.type;
-	inode->attr.mode = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.mode;
-	inode->attr.nlink = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.nlink;
-	inode->attr.uid = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.uid;
-	inode->attr.gid = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.gid;
-	inode->attr.size = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.size;
-	inode->attr.used = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.used;
-	inode->attr.rdev.major = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.rdev.major;
-	inode->attr.rdev.minor = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.rdev.minor;
-	inode->attr.fsid = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.fsid;
-	inode->attr.fileid = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.fileid;
+	fattr = &res.wccstat3_u.wcc.after.post_op_attr_u.attributes;
+	inode->attr.type = fattr->type;
+	inode->attr.mode = fattr->mode;
+	inode->attr.nlink = fattr->nlink;
+	inode->attr.uid = fattr->uid;
+	inode->attr.gid = fattr->gid;
+	inode->attr.size = fattr->size;
+	inode->attr.used = fattr->used;
+	inode->attr.rdev.major = fattr->rdev.major;
+	inode->attr.rdev.minor = fattr->rdev.minor;
+	inode->attr.fsid = fattr->fsid;
+	inode->attr.fileid = fattr->fileid;
  
-	inode->attr.atime.seconds = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.atime.seconds;
-	inode->attr.atime.nseconds = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.atime.nseconds;
-	inode->attr.mtime.seconds = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.mtime.seconds;
-	inode->attr.mtime.nseconds = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.mtime.nseconds;
-	inode->attr.ctime.seconds = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.ctime.seconds;
-	inode->attr.ctime.nseconds = res.wccstat3_u.wcc.after.post_op_attr_u.attributes.ctime.nseconds;
-	
+	inode->attr.atime.seconds = fattr->atime.seconds;
+	inode->attr.atime.nseconds = fattr->atime.nseconds;
+	inode->attr.mtime.seconds = fattr->mtime.seconds;
+	inode->attr.mtime.nseconds = fattr->mtime.nseconds;
+	inode->attr.ctime.seconds = fattr->ctime.seconds;
+	inode->attr.ctime.nseconds = fattr->ctime.nseconds;
+
+	clnt_freeres(clntp, (xdrproc_t)xdr_wccstat3, (char *)&res);
  out:
 	DEBUG_OUT("Leave hsi_nfs3_setattr() with errno : %d.\n", err);
-	clnt_freeres(clntp, (xdrproc_t)xdr_wccstat3, (char *)&res);
 	return err;
 }
