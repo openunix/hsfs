37aa55a8a362827db05f074c480960e4e5ee72cb
xuehaitao <xuehaitao@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-12 11:03:44 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8741 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_symlink.c
+++ fuse/hsx_fuse_symlink.c
@@ -8,35 +8,42 @@
 #include "hsi_nfs3.h"
 #include "hsx_fuse.h"
 #include "log.h"
+#include <errno.h>
 
 void hsx_fuse_symlink(fuse_req_t req, const char *link,
                       fuse_ino_t parent, const char *name)
 {
 	int st = 0;
 	int err = 0;
-	struct hsfs_super *sb_parent;
-	struct hsfs_inode *nfs_parent;
+	struct hsfs_super *sb_parent = NULL;
+	struct hsfs_inode *nfs_parent = NULL;
 	struct hsfs_inode *new = NULL;
-	struct fuse_entry_param *e = NULL;
-	e = (struct fuse_entry_param *)malloc(sizeof(struct fuse_entry_param));
+	struct fuse_entry_param e;
+
 	DEBUG_IN("%s\n","hsx_fuse_symlink.");
 
 	sb_parent = fuse_req_userdata(req);
+	if(!sb_parent){
+                ERR("%s gets inode->sb fails \n",progname);
+                err=ENOENT;
+                goto out;
+        }
+
 	nfs_parent = hsx_fuse_iget(sb_parent, parent);
+	if(!nfs_parent){
+                ERR("%s gets inode fails \n",progname);
+                err=ENOENT;
+                goto out;
+        }
 
 	st = hsi_nfs3_symlink(nfs_parent, &new, link, name);
 	if(st != 0){
 		err = st;
 		goto out;
 	}
-	hsx_fuse_fill_reply(new, e);
-	st = fuse_reply_entry(req, e);
-	if(st != 0){
-		err = st;
-		goto out;
-	}
+	hsx_fuse_fill_reply(new, &e);
+	fuse_reply_entry(req, &e);
 out:
-	free(e);
 	if(err != 0){
 		fuse_reply_err(req, err);
 	}
