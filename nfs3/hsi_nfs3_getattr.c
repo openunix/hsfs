e5ea95bc1309eadbba782ca4700ec220d113a1f2
zhaoyan <zhaoyan@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-12 10:44:47 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8733 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- nfs3/hsi_nfs3_getattr.c
+++ nfs3/hsi_nfs3_getattr.c
@@ -19,8 +19,8 @@ int hsi_nfs3_getattr(struct hsfs_inode *inode)
 	fh.data.data_val = inode->fh.data.data_val;
 	memset(&res, 0, sizeof(res));
 	st = clnt_call(clntp, NFSPROC3_GETATTR, (xdrproc_t)xdr_nfs_fh3, 
-		(caddr_t)&fh, (xdrproc_t)xdr_getattr3res, (caddr_t)&res,
-		to);
+		       (caddr_t)&fh, (xdrproc_t)xdr_getattr3res, (caddr_t)&res,
+		       to);
 	if (st) {
 		err = hsi_rpc_stat_to_errno(clntp);
 		ERR("Call RPC Server failure: %d.\n", err);
