d250bd0832cc3746b684a8a28ab8c7ac3ad330e6
liuyoujin <liuyoujin@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-12 03:21:23 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8641 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_statfs.c
+++ fuse/hsx_fuse_statfs.c
@@ -2,20 +2,24 @@
  * hsx_fuse_statfs
  * liuyoujin
  */
-#include <stdio.h>
-#include <unistd.h>
+//#include <stdio.h>
+//#include <unistd.h>
 #include <sys/errno.h>
 #include "hsi_nfs3.h"
 #include "hsx_fuse.h"
-#include "log.h"
+//#include "log.h"
 extern void hsx_fuse_statfs(fuse_req_t req, fuse_ino_t ino)
 {
-DEBUG_IN("%s","...");
 	struct statvfs stbuf;
-        struct hsfs_super *super = fuse_req_userdata(req);
-	struct hsfs_inode *root = super->root;
-	struct hsfs_super *sp = root->sb;
+        struct hsfs_super *super = NULL;
+	struct hsfs_inode *root = NULL;
+	struct hsfs_super *sp = NULL;
 	int err=0;
+	DEBUG_IN(" err:%d", err);
+
+        super = fuse_req_userdata(req);
+	root = super->root;
+	sp = root->sb;
 	err = hsi_nfs3_statfs(root);
         if (err){
 		fuse_reply_err (req, err);
@@ -29,7 +33,7 @@ DEBUG_IN("%s","...");
 	}
 	fuse_reply_statfs (req, &stbuf);
 out:
-DEBUG_OUT ("%s","...");
+	DEBUG_OUT (" err:%d",err);
 	return;
 
 }
