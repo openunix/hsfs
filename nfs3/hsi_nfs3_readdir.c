50c882de4884e0cc9c9c1c9069ebeff755f5a979
huangyongsheng <huangyongsheng@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-12 10:20:40 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8717 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- nfs3/hsi_nfs3_readdir.c
+++ nfs3/hsi_nfs3_readdir.c
@@ -10,112 +10,124 @@
 int hsi_nfs3_readdir(struct hsfs_inode *hi, struct hsfs_readdir_ctx *hrc, 
 					size_t *dircount, size_t maxcount)
 {
-	DEBUG_IN("%s.","hsx_fuse_readdir");
 	CLIENT *clntp = NULL;
-	struct hsfs_readdir_ctx *temp = NULL; 
-	struct hsfs_readdir_ctx *temp1 = NULL;
+	struct hsfs_readdir_ctx *temp_hrc = NULL; 
+	struct hsfs_readdir_ctx *temp_hrc1 = NULL;
 	struct readdirplus3resok *resok = NULL;
+	struct entryplus3 *temp_entry = NULL;
 	struct readdirplus3args args;
 	struct readdirplus3res res;
-	struct rpc_err rerr;
-	struct timeval to = {10, 0};
+	struct timeval to;
 	size_t dir_size = 0;
 	int err = 0;
 	int flag = 0;
 
-	temp1 = hrc;
+	DEBUG_IN("%s.","hsx_fuse_readdir");
 	memset(&args, 0, sizeof(args));
 	memset(&res, 0, sizeof(res));
 	args.cookie = hrc->off;
 	memcpy(args.cookieverf, hrc->cookieverf, NFS3_COOKIEVERFSIZE);
 	args.dir.data.data_val = hi->fh.data.data_val;
-        args.dir.data.data_len = hi->fh.data.data_len;
+	args.dir.data.data_len = hi->fh.data.data_len;
 	args.maxcount = maxcount;
-	dircount = (size_t *)&args.dircount;
-	args.dircount = 1024;
+	dircount = (size_t *)&(args.dircount);
+	temp_hrc1 = hrc;
+	to.tv_sec = hi->sb->timeo / 10;
+	to.tv_usec = (hi->sb->timeo % 10) * 100000;
 	clntp = hi->sb->clntp;
 
-	do{
-		
+	do{		
 		err = clnt_call(clntp, NFSPROC3_READDIRPLUS,
 				(xdrproc_t)xdr_readdirplus3args, &args,
 				(xdrproc_t)xdr_readdirplus3res, &res, to);
 		if (err) {	
+			ERR("Call RPC Server failure:(%s).\n", 
+							clnt_sperrno(err));
 			err = hsi_rpc_stat_to_errno(hi->sb->clntp);
 			goto out;
 		}
 
 		if (NFS3_OK != res.status) {
+			ERR("Call NFS3 Server failure:(%d).\n", err);
 			err = hsi_nfs3_stat_to_errno(res.status);
+			clnt_freeres(clntp, (xdrproc_t)xdr_readdirplus3res,
+                                                                (char *)&res);
 			goto out;
 		}
  
 		resok = &res.readdirplus3res_u.resok;
-		while(resok->reply.entries != NULL){
+		temp_entry = resok->reply.entries;
+		while(temp_entry != NULL){
 			if (flag == 1){
-				temp->off = resok->reply.entries->cookie;
+				temp_hrc->off = temp_entry->cookie;
 				flag = 0; 
 			}
 
-		 	temp = (struct hsfs_readdir_ctx *)malloc(sizeof
-				(struct hsfs_readdir_ctx));
-			memset(temp, 0, sizeof(struct hsfs_readdir_ctx));
-			temp->name = (char *)malloc(strlen
-					(resok->reply.entries->name) + 1);
-			if (temp->name == NULL) {
-				ERR("Memory leak.");
+			temp_hrc = (struct hsfs_readdir_ctx *)malloc(sizeof
+						(struct hsfs_readdir_ctx));
+			if(temp_hrc == NULL){
+				ERR("Temp_hrc memory leak.");
 				goto out;
 			}
 
-			memcpy(temp->cookieverf, resok->cookieverf,
+			memset(temp_hrc, 0, sizeof(struct hsfs_readdir_ctx));
+			temp_hrc->name = (char *)malloc(strlen
+						(temp_entry->name) + 1);
+			if (temp_hrc->name == NULL) {
+				ERR("Temp_hrc->name memory leak.");
+				goto out;
+			}
+
+			memcpy(temp_hrc->cookieverf, resok->cookieverf,
 						 NFS3_COOKIEVERFSIZE);
-			strcpy(temp->name, resok->reply.entries->name);
-			if (resok->reply.entries->nextentry != NULL)
-			  	 temp->off = resok->reply.entries->nextentry
-				->cookie;
+			strcpy(temp_hrc->name, temp_entry->name);
+			if (temp_entry->nextentry != NULL)
+				temp_hrc->off = temp_entry->nextentry->cookie;
 			else{
 				err = resok->reply.eof;
-			  	if (err == 0){
+				if (err == 0){
 					flag = 1;
-			      		args.cookie = 
-						resok->reply.entries->cookie;
-			     		memcpy(args.cookieverf, 
+					args.cookie = temp_entry->cookie;
+					memcpy(args.cookieverf, 
 							resok->cookieverf, 
 							NFS3_COOKIEVERFSIZE);
 				}
 				else{
-					temp->off =  
-						resok->reply.entries->cookie;
+					temp_hrc->off =  temp_entry->cookie;
 				}					
 			}		
 	 
-			err = resok->reply.entries->name_attributes.present;
+			err = temp_entry->name_attributes.present;
 			if (1 != err) {
-				ERR("Get Fattr3 failure:%s", clnt_sperrno(err));
-				clnt_geterr(hi->sb->clntp, &rerr);
-				err = rerr.re_errno;
+				ERR("Rpc get fattr3 failure: %s.", 
+							clnt_sperrno(err));
 				goto out;
 			}
 	    		
-			hsi_nfs3_fattr2stat(&resok->reply.entries->						name_attributes.post_op_attr_u.attributes, 									&temp->stbuf);
+			err = hsi_nfs3_fattr2stat(&temp_entry->							name_attributes.post_op_attr_u.attributes, 								&temp_hrc->stbuf);
 			
-			hrc->next = temp;
-			hrc = temp;
-			resok->reply.entries = resok->reply.entries->nextentry;
+			if(err != 0){
+				ERR("Set fattr3 to stat failure.");
+				goto out;
+	
+			}
+			temp_hrc1->next = temp_hrc;
+			temp_hrc1 = temp_hrc;
+			temp_entry = temp_entry->nextentry;
 		}
 	  
 		dir_size += *dircount;
 		if (dir_size > maxcount)
 			break;
+
 		clnt_freeres(clntp, (xdrproc_t)xdr_readdirplus3res, 
-						(char *)&res);
+								(char *)&res);
 
 		err = resok->reply.eof;
 	}while(err == 0);
  
 	err = 0;
-	hrc = temp1;
-	
+
 out:
 	DEBUG_OUT("%s.","hsx_fuse_readdir");
 	return err;
