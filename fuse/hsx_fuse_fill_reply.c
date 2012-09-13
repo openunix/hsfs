5c25ed90f887e22ab11a6e2e1c59a413f3fd3d73
huyuwei <huyuwei@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-13 02:21:29 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8765 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- fuse/hsx_fuse_fill_reply.c
+++ fuse/hsx_fuse_fill_reply.c
@@ -1,5 +1,6 @@
 #include "hsi_nfs3.h"
 #include "hsfs.h"
+#include "nfs3.h"
 /**
  *  hsx_fuse_fill_reply
  *  fill the struct fuse_entry_param with struct hsfs_inode.fattr3
@@ -7,8 +8,6 @@
  *  Edit:2012/09/03 Hu yuwei
  *  
  */
-#define S_ISREG 1
-#define S_ISDIR 2
 void hsx_fuse_fill_reply (struct hsfs_inode *inode, struct fuse_entry_param *e)
 {	
 	DEBUG_IN(" in %s\n", "hsx_fuse_fill_reply");
@@ -18,11 +17,11 @@ void hsx_fuse_fill_reply (struct hsfs_inode *inode, struct fuse_entry_param *e)
 	}
 	if (0 == hsi_nfs3_fattr2stat(&(inode->attr), &(e->attr))) {
 		e->ino = inode->ino;
-		if (S_ISREG == inode->attr.type) {
+		if (NF3REG == inode->attr.type) {
 			e->attr_timeout = inode->sb->acregmin;
 			e->entry_timeout = inode->sb->acregmax;
 		}
-		else if (S_ISDIR == inode->attr.type) {
+		else if (NF3DIR == inode->attr.type) {
 			e->attr_timeout = inode->sb->acdirmin;
 			e->entry_timeout = inode->sb->acdirmax;
 		}else
