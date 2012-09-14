3c8026ad25f31ce88d28f67e89adb823689f70a4
huyuwei <huyuwei@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-14 06:24:52 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8812 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- nfs3/hsi_nfs3_mkdir.c
+++ nfs3/hsi_nfs3_mkdir.c
@@ -28,7 +28,7 @@ int hsi_nfs3_mkdir (struct hsfs_inode *hi_parent, struct hsfs_inode **hi_new,
 	struct timeval timeout = { hi_parent->sb->timeo/10,
 	       	(hi_parent->sb->timeo/10)*100000};	
 	
-	DEBUG_IN(" in %s.\n","hsi_nfs3_mkdir");
+	DEBUG_IN(" ino: %lu.\n", hi_parent->ino);
 	memset(&argp, 0, sizeof(mkdir3args));
 	memset(&clnt_res, 0, sizeof(diropres3));
 	argp.where.dir.data.data_len = hi_parent->fh.data.data_len;
