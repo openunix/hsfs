e9b6895fef16ade75ec1e7bd998c551fcb74c45e
huyuwei <huyuwei@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-18 08:39:52 +0000

    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8885 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- nfs3/hsi_acl3.c
+++ nfs3/hsi_acl3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+
+#include "hsfs.h"
 #include "acl.h"
-#include "log.h"
 
 #define atomic_set(v,i)		((v)->counter = (i))
 /*
@@ -47,3 +48,90 @@ int posix_acl_to_xattr(const struct posix_acl *acl, void *buffer, size_t size)
 	return real_size;
 }
 
+/*
+ *Convert from in-memory to acl
+ *
+ */
+
+struct posix_acl *
+posix_acl_from_xattr(const void *value, size_t size)
+{
+	posix_acl_xattr_header *header = (posix_acl_xattr_header *)value;
+	posix_acl_xattr_entry *entry = (posix_acl_xattr_entry *)(header+1),
+*end;
+	int count;
+	size_t getcount = size;
+	struct posix_acl *acl;
+	struct posix_acl_entry *acl_e;
+
+	if (!value)
+		return NULL;
+	if (size < sizeof(posix_acl_xattr_header))
+		 return NULL;
+	if (header->a_version != POSIX_ACL_XATTR_VERSION)
+		return NULL;
+
+	if (getcount < sizeof(posix_acl_xattr_header))
+		return NULL;
+	getcount -= sizeof(posix_acl_xattr_header);
+	if (getcount % sizeof(posix_acl_xattr_entry))
+		return NULL;          
+	count = getcount / sizeof(posix_acl_xattr_entry);
+	if (count < 0)
+		return NULL;
+	if (count == 0)
+		return NULL;
+	
+	acl = posix_acl_alloc(count);
+	if (!acl)
+		return NULL;
+	acl_e = acl->a_entries;
+	
+	for (end = entry + count; entry != end; acl_e++, entry++) {
+		acl_e->e_tag  = entry->e_tag;
+		acl_e->e_perm = entry->e_perm;
+
+		switch(acl_e->e_tag) {
+			case ACL_USER_OBJ:
+			case ACL_GROUP_OBJ:
+			case ACL_MASK:
+			case ACL_OTHER:
+				acl_e->e_id = ACL_UNDEFINED_ID;
+				break;
+
+			case ACL_USER:
+			case ACL_GROUP:
+				acl_e->e_id = entry->e_id;
+				break;
+
+			default:
+				goto fail;
+		}
+	}
+	return acl;
+
+fail:
+	free(acl);
+	return NULL;
+}
+
+struct posix_acl *
+posix_acl_from_mode(mode_t mode)
+{
+	struct posix_acl *acl = posix_acl_alloc(3);
+	if (!acl)
+		return NULL;
+
+	acl->a_entries[0].e_tag  = ACL_USER_OBJ;
+	acl->a_entries[0].e_id   = ACL_UNDEFINED_ID;
+	acl->a_entries[0].e_perm = (mode & S_IRWXU) >> 6;
+
+	acl->a_entries[1].e_tag  = ACL_GROUP_OBJ;
+	acl->a_entries[1].e_id   = ACL_UNDEFINED_ID;
+	acl->a_entries[1].e_perm = (mode & S_IRWXG) >> 3;
+
+	acl->a_entries[2].e_tag  = ACL_OTHER;
+	acl->a_entries[2].e_id   = ACL_UNDEFINED_ID;
+	acl->a_entries[2].e_perm = (mode & S_IRWXO);
+	return acl;
+}
