e2be76048dba8cb53c22556c2e9d20c070d4125c
yanhuan <yanhuan@91053cd5-a235-4db3-b8ea-753d4ab5bda5>
2012-09-18 08:14:42 +0000

    change comment in hsi_nfs3.h
    git-svn-id: http://svn.bwstor.com.cn/svn/client/hsfs/trunk@8883 91053cd5-a235-4db3-b8ea-753d4ab5bda5

--- include/hsi_nfs3.h
+++ include/hsi_nfs3.h
@@ -5,11 +5,11 @@
 #include "acl3.h"
 #include "acl.h"
 /**
- * @brief make a directory
+ * @brief Make a directory
  *
  * @param parent[in] the information of the source parent directory
  * @param name[in] directory name
- * @param new[in] the information of the target directory
+ * @param new[out]] the information of the target directory
  * @param mode[in] the mode of the target directory
  *
  * @return error number
@@ -17,7 +17,7 @@
 extern int hsi_nfs3_mkdir(struct hsfs_inode *parent, struct hsfs_inode **new,
 	       		const char *name, mode_t mode);
 /**
- * @brief remove a directory
+ * @brief Remove a directory
  *
  * @param parent[in] the information of the source parent directory
  * @param name[in] directory name
@@ -106,7 +106,7 @@ extern int hsi_nfs3_read(struct hsfs_rw_info* rw);
 /**  
  * @brief Write file
  *
- * @param rw[int,out] the content of the write operation 
+ * @param rw[in,out] the content of the write operation 
  *
  * @return error number
  **/
@@ -116,25 +116,25 @@ extern int hsi_nfs3_write(struct hsfs_rw_info* rw);
 /**
  * @brief Request permission to an operation.
  *
- * @param hi[in] the information of the inode requested
+ * @param inode[in] the information of the inode requested
  * @param mask[in] the permission requested
  *
  * return error number
  **/
-extern int hsi_nfs3_access(struct hsfs_inode *hi, int mask);
+extern int hsi_nfs3_access(struct hsfs_inode *inode, int mask);
 
 
 /**
  * @brief Create a regular file.
  *
- * @param hi[in] the information of the parent directory
- * @param newhi[out] the information of the file created
+ * @param parent[in] the information of the parent directory
+ * @param new[out] the information of the file created
  * @param name[in] the name of the file you want to create
  * @param mode[in] the access mode of the file
  *
  * @return error number
  */
-extern int hsi_nfs3_create(struct hsfs_inode *hi, struct hsfs_inode **newhi,
+extern int hsi_nfs3_create(struct hsfs_inode *parent, struct hsfs_inode **new,
 			   const char *name, mode_t mode);
 
 
@@ -142,7 +142,7 @@ extern int hsi_nfs3_create(struct hsfs_inode *hi, struct hsfs_inode **newhi,
  *  @brief Create a special file
  *
  *  @param parent[in] the inode of the parent directory
- *  @param newinode[out] the inode of the created file
+ *  @param new[out] the inode of the created file
  *  @param name[in] the name to create
  *  @param mode[in] file type and access mode of the file
  *  @param rdev[in] device number of the file: only used for CHR and BLK
@@ -150,54 +150,54 @@ extern int hsi_nfs3_create(struct hsfs_inode *hi, struct hsfs_inode **newhi,
  *  @return error number
  */
 extern	int hsi_nfs3_mknod(struct hsfs_inode *parent, struct hsfs_inode 
-		  	**newinode, const char *name,mode_t mode, dev_t rdev);
+		  	**new, const char *name,mode_t mode, dev_t rdev);
 
 /**
  * @brief Create a hard link to a file
  *
- * @param ino[in] the inode of the file
+ * @param inode[in] the inode of the file
  * @param newparent[in] the inode of the new parent
- * @param newinode[out] the inode of the linked file
+ * @param new[out] the inode of the linked file
  * @param name[in] the name to of the link to create
  * 
  * @return error number
  */
 extern 	int hsi_nfs3_link(struct hsfs_inode *inode, struct hsfs_inode *newparent, 
-			struct hsfs_inode **newinode ,  const char *name);
+			struct hsfs_inode **new ,  const char *name);
 
 /**
  * @brief Look up a directory entry by name
  *
- * @param parent[IN] The information of the parent node
- * @param newinode[OUT] Point to the node information obtained by name
- * @param name[IN] The name to look up
+ * @param parent[in] The information of the parent node
+ * @param new[out] Point to the node information obtained by name
+ * @param name[in] The name to look up
  *
  * @return error number
  * */
 extern  int hsi_nfs3_lookup(struct hsfs_inode *parent, struct hsfs_inode 
-			**newinode, const char *name);
+			**new, const char *name);
 
 /**
  * @brief Allocate memory for a node
  *
- * @param sb[IN] the information of the superblock
- * @param pfh[IN] a pointer to the filehandle of node 
- * @param ino[IN] the inode number
- * @param pattr[IN] a pointer to the attributes of node 
+ * @param sb[in] the information of the superblock
+ * @param fh[in] a pointer to the filehandle of node 
+ * @param ino[in] the inode number
+ * @param attr[in] a pointer to the attributes of node 
  *
  * @return a pointer to the object if successfull,else NULL
  * */
 extern struct hsfs_inode *hsi_nfs3_alloc_node(struct hsfs_super *sb, nfs_fh3 
-					*pfh, uint64_t ino, fattr3 *pattr);
+					*fh, uint64_t ino, fattr3 *attr);
 
 /**
  * @brief Look up a node from hash table
  *
- * @param sb[IN] The information of the superblock
- * @param fh[IN] Filehandle of the parent directory
- * @param attr[IN] Node attributes
+ * @param sb[in] The information of the superblock
+ * @param fh[in] Filehandle of the parent directory
+ * @param attr[in] Node attributes
  *
- * @return A pointer to the node information
+ * @return a pointer to the node information
  * */
 extern struct hsfs_inode *hsi_nfs3_ifind(struct hsfs_super *sb, nfs_fh3 *fh, 
 					 fattr3 *attr);
@@ -214,12 +214,12 @@ extern int hsi_nfs3_getattr(struct hsfs_inode *inode);
 /**
  * @brief Convert struct fattr3 to struct stat
  *                                                                                                                                         
- * @param fattr[in] the attributes 
+ * @param attr[in] the attributes 
  * @param st[out] the result of the convertion
  * 
  * @return error number
  */
-extern int hsi_nfs3_fattr2stat(struct fattr3 *fattr, struct stat *st);
+extern int hsi_nfs3_fattr2stat(struct fattr3 *attr, struct stat *st);
 
 /**
  * @brief Convert struct stat to struct hsfs_sattr
@@ -243,7 +243,7 @@ extern int hsi_nfs3_stat2sattr(struct stat *st, int to_set,
 extern int hsi_nfs3_setattr(struct hsfs_inode *inode, struct hsfs_sattr *attr);
 
 /**
- * @Get Dynamic file system information
+ * @brief Get Dynamic file system information
  *
  * @param inode[in,out] the information of inode
  * @return error number
@@ -269,33 +269,33 @@ extern int hsi_nfs3_readdir(struct hsfs_inode *parent, struct hsfs_readdir_ctx
 						*hrc, size_t maxcount);
 
 /**
- * @Get static file system information of NFS
+ * @breif Get static file system information of NFS
  *
- * @inode: hsfs inode of root directory
+ * @param inode[in]  hsfs inode of root directory
  *
  * @return errno number as Linux system
  */
 extern int hsi_nfs3_fsinfo(struct hsfs_inode *inode);
 
 /**
- * @Retrieve POSIX information of NFS
+ * @brief Retrieve POSIX information of NFS
  *
- * @inode: hsfs inode of root directory
+ * @param inode[in] hsfs inode of root directory
  *
  * @return errno number as Linux system
  */
 extern int hsi_nfs3_pathconf(struct hsfs_inode *inode);
 
 /**
- * @Potting clnt_call
+ * @brief Potting clnt_call
  *
- * @sb: super block of hsfs
- * @clnt: CLIENT info
- * @procnum: NFS procedure number(macro defined in nfs3.h)
- * @inproc: function which is used to encode the procedure's parameters
- * @in: the address of the procedure's argument(s)
- * @outproc: function which is used to decode the procedure's results
- * @out:	the address of where to place the result(s)
+ * @param[in] sb super block of hsfs
+ * @param[in] clnt CLIENT info
+ * @param[in] procnum NFS procedure number(macro defined in nfs3.h)
+ * @param[in] inproc function which is used to encode the procedure's parameters
+ * @param[in] in the address of the procedure's argument(s)
+ * @param[in] outproc function which is used to decode the procedure's results
+ * @param[out] out	the address of where to place the result(s)
  *
  * @return errno number as Linux system
  */
