d3aedc50b95472e2b18a9af9f6df05c86978b9e4
Feng Shuo <steve.shuo.feng@gmail.com>
2012-10-29 09:30:58 +0800

    Add license and reviewed 50% of all the files.

--- fuse/hsx_fuse_setattr.c
+++ fuse/hsx_fuse_setattr.c
@@ -1,8 +1,27 @@
+/*
+ * Copyright (C) 2012 Zhao Yan, Feng Shuo <steve.shuo.feng@gmail.com>
+ *
+ * This file is part of HSFS.
+ *
+ * HSFS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HSFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HSFS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <errno.h>
 #include "hsi_nfs3.h"
 
 void hsx_fuse_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr, 
-		int to_set, struct fuse_file_info *fi)
+		      int to_set, struct fuse_file_info *fi)
 {
   	int err = 0;
 	double to = 0;
@@ -19,6 +38,7 @@ void hsx_fuse_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
 		ERR("req is invalid.\n");
 		goto out;
 	}
+
 	inode = hsx_fuse_iget(sb, ino);
 	if (NULL == inode) {
 		err = ENOENT;
@@ -30,7 +50,9 @@ void hsx_fuse_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
 		goto out;
 	err = hsi_nfs3_setattr(inode, &sattr);
 	if (err)
-		goto out;		
+		goto out;
+
+	/* XXX Review to here! */
 	memset(&st, 0, sizeof(st));
 	err = hsi_nfs3_fattr2stat(&inode->attr, &st);
  out:
--- nfs3/hsi_nfs3_stat2iattr.c
+++ nfs3/hsi_nfs3_stat2iattr.c
@@ -1,8 +1,27 @@
+/*
+ * Copyright (C) 2012 Yan Huan, Feng Shuo <steve.shuo.feng@gmail.com>
+ *
+ * This file is part of HSFS.
+ *
+ * HSFS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HSFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HSFS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <errno.h>
 #include "hsi_nfs3.h"
 
 int hsi_nfs3_stat2sattr(struct stat *st, int to_set, 
-			struct hsfs_sattr *attr)
+			struct hsfs_iattr *attr)
 {
   	int err = 0;
   	
