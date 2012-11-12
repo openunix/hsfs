76da99c45b2920998d73dabaff1fc7699a37c038
Feng Shuo <steve.shuo.feng@gmail.com>
2012-11-12 17:22:57 +0800

    Change to UNix format

--- include/acl.h
+++ include/acl.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2012 Hu Yuwei, Shou Xiaoyun,
+ *	Feng Shuo <steve.shuo.feng@gmail.com>
+ *
+ * This file is part of HSFS.
+ *
+ * HSFS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HSFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HSFS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __ACL_H
 #define __ACL_H
 
